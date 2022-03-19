7acc8c52f813aabdaf003094037de37202f3aebf
Maneet-K <maneetkaur797@gmail.com>
2022-03-19 13:38:19 +0530

    update navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" href="/">News Ghanta🔔</Link>
+                    <Link className="navbar-brand" href="/">News Ghanta🕐</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
